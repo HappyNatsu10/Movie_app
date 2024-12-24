0afe9dfebc4c4ef61bf9b763c10a07533654d5f0
HappyNatsu10 <afilakapeter@gmail.com>
2024-12-24 22:54:25 +0100

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "fbb7689223d07a475b0cb3760f3855f2";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
